83ed03f34b82cbf94cec0efcfd3253f47057d735
Sergio <sergio@MacBook-Pro-de-Sergio.local>
2014-02-09 13:45:56 +0100

    Inicio de repositorio

---
+++ SpriteAnimationVegeta/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteAnimationVegeta
+//
+//  Created by Sergio on 09/02/14.
+//  Copyright (c) 2014 Sergio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpriteAnimationVegeta/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  SpriteAnimationVegeta
+//
+
+//  Copyright (c) 2014 Sergio. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
